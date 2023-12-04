e90170421ac60615ac09d9bca56654136aff9bdf
kkazu <>
2023-12-04 23:01:06 +0900

    画面表示

--- backend/server.ts
+++ backend/server.ts
@@ -9,6 +9,31 @@ app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello, world!");
 });
 
+app.get("/api", (req: express.Request, res: express.Response) => {
+  res.json([
+    {
+      id:1,
+      name:"りんご",
+      price:200,
+    },
+    {
+      id:2,
+      name:"バナナ",
+      price:300,
+    },
+    {
+      id:3,
+      name:"みかん",
+      price:"150",
+    },
+    {
+      id:4,
+      name:"メロン",
+      price:"2000",
+    },
+  ]);
+});
+
 app.listen(port, () => {
   console.log(`port ${port} でサーバー起動中`);
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+type Fruit = {
+  id: number;
+  name: string;
+  price: number;
+};
+
 function App() {
+  const [fruits, setFruits] = useState<Fruit[] | null>(null);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        /* ↓「http://localhost:8000」を消す */
+        const res = await fetch("/api");
+        const json: React.SetStateAction<Fruit[] | null> = await res.json();
+        setFruits(json);
+      } catch (e: unknown) {
+        if (e instanceof Error) {
+          console.error(e.message);
+        }
+      }
+    };
+
+    fetchData();
+  }, []); // 空の依存配列は、useEffectがマウント時に一度だけ実行されることを保証します
+
   return (
-    <div className="App">
-      <input type= "text"></input>
-      <button>追加</button>
+    <div className="container fruitsList">
+      <h1>Fruits Store</h1>
+
+      {fruits?.map((fruit) => (
+        <div key={fruit.id}>
+          <div className="text">
+            <h2>{fruit.name}</h2>
+            <p>{`￥${fruit.price}`}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
